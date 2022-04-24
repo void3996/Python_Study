7b1e66fbfb3738de2932a2bc55b8ee46664704aa
void3996 <rorlfkf@gmail.com>
2022-04-25 00:08:46 +0900

    2 day

---
+++ test/Number.py
@@ -0,0 +1,62 @@
+    #숫자형
+    #정수형
+a = 123
+print("a = ", a)
+b = -178
+print("b = ", b )
+c = 0
+print("c = ", c)
+
+    #실수형
+a1 = 1.2
+b1 = -3.45
+print("a1 = ", a1)
+print("b1 = ", b1)
+
+    #컴퓨터식 지수 표현방식
+a2 = 4.24E10
+b2 = 4.24e-10
+print("a2 = ", a2)
+print("b2 = ", b2)
+
+    #8진수와 16진수
+a3 = 0o177
+print("a3 = ", a3)
+
+    #16진수
+a4 = 0x8ff
+b4 = 0xABC
+print("a4 = ", a4)
+print("b4 = ", b4)
+
+    #연산자
+    #사칙연산
+a = 3
+b = 4
+print("a + b = ", a+b)
+print("a * b = ", a*b)
+print("a / b = ", a/b)
+
+    ##x의 y 제곱을 나타내는 ** 연산자
+
+print("a ** b = ", a**b)
+
+    ##나머지 반환 연산자 %
+
+a = 7
+b = 3
+
+print("a % b = ", a%b)
+print("b % a = ", b%a)
+
+    #몫 반환 연산자 //
+
+a = 7
+b = 4
+print("a/b = ", a/b)
+print("a//b = ", a//b)
+
+a = 14
+b = 3
+print("a//b =", a//b)
+print("a%b =", a%b)
--- test/hello.py
+++ test/hello.py
@@ -1,1 +1,2 @@
 print("hello world!")
+print("hellow world!!")
