958f6517eb147c2341791d45fe741aeb021dd6fb
void3996 <rorlfkf@gmail.com>
2022-04-25 00:40:31 +0900

    2day

---
+++ test/String.py
@@ -0,0 +1,56 @@
+print("Hello World")
+
+print('Python is fun')
+
+print("""Life is to short, You need python""")
+
+print('''Life is to short, You need python''')
+
+food = "Python's favorite food is perl"
+print(food)
+
+say = '"Python is very easy." he says.'
+print(say)
+
+food = 'Python\'s favorite food is perl'
+say = "\"Python is very easy.\" he says."
+
+print(food)
+print(say)
+
+multiline = "Life is too short\nYou need python"
+print(multiline)
+
+multiline2 = '''
+Life is too short
+You need python
+'''
+print(multiline2)
+
+multiline3 = """
+Life is too short
+You need python
+"""
+print(multiline3)
+
+    #문자열 연산하기
+
+head = "Python"
+tail = " is fun!"
+print(head + tail)
+
+a = "python"
+print("a*2 = ", a*2)
+
+print("=" * 50)
+print("My Program")
+print("="*50)
+
+    #문자열 길이 구하기
+a = "Life is too short"
+print("len(a) = ", len(a))
+
+a = "You need python"
+print("a의 문자열 길이는 ", len(a) ,"이다.")
+
+    #문자열 인덱싱과 슬라이싱   
