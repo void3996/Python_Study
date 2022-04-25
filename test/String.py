b09f5156d4d485b2a7b2ecdbfab743baa374aaa4
void3996 <rorlfkf@gmail.com>
2022-04-26 00:43:19 +0900

    3day

--- test/String.py
+++ test/String.py
@@ -54,3 +54,40 @@ a = "You need python"
 print("a의 문자열 길이는 ", len(a) ,"이다.")
 
     #문자열 인덱싱과 슬라이싱   
+
+a = "Life is too short, You need Python"
+print("a[3] = ", a[3])
+    #파이썬은 0부터 숫자를 센다
+print("a[12] = ", a[12])
+print("a[-1] = ", a[-1])
+
+print("a[0:4] = ", a[0:4])
+
+a = "20010331Rainy"
+date = a[:8]
+weather = a[8:]
+print("date = ", date)
+print("weather = ", weather)
+
+a = "Pithon"
+a[:1]
+print(a[:1])
+print(a[2:])
+
+b = a[:1] + 'y' + a[2:]
+print(b)
+
+    #문자열 포매팅 따라하기
+    #숫자 바로 대입
+a = "I eat %d apples." % 3
+print(a)
+
+    #문자열 바로 대입
+a = "I eat %s apples." % "five"
+print(a)
+
+    #숫자 값을 나타내는 변수로 대입
+number = 3
+a = "I eat %d apples." % number
+print(a)
+
