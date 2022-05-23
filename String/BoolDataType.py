8e79d927a43a4172b23004e1350d5aad2e5006ec
void3996 <rorlfkf@gmail.com>
2022-05-23 23:06:36 +0900

    DataType

---
+++ String/BoolDataType.py
@@ -0,0 +1,27 @@
+a = True
+b = False
+type(a)
+type(b)
+
+1==1
+
+2>1
+2<1
+
+a = [1, 2, 3, 4]
+while a:
+     a.pop()
+  
+
+if [1, 2, 3]:
+     print("참")
+else:
+     print("거짓")
+    
+
+
+num = 1
+while num <= 100:
+      print(num)
+      num = num + 1
+  
