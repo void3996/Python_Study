89aae58cb1a1719970be685dfbf833faf9a6fa56
void3996 <rorlfkf@gmail.com>
2022-06-15 20:58:27 +0900

    commit

--- I-O/Exercise.py
+++ I-O/Exercise.py
@@ -28,13 +28,17 @@ def avg_numbers(*avg):
 
 avg_numbers(1, 2)
 avg_numbers(1,2,3,4,5)
+avg_numbers(1,2,3,4,5,6,6,7,8,8)
 
 #Q3 다음은 두 개의 숫자를 입력받아 더하여 돌려주는 프로그램이다.
 input1 = input("첫번째 숫자를 입력하세요:")
 input2 = input("두번째 숫자를 입력하세요:")
 
 total = int(input1) + int(input2)
+total2 = input1 + input2
 print("두 수의 합은 %s 입니다." % total)
+print("두 수의 합은 %s 입니다." % total2)
+
 
 #Q4 다음 중 출력 결과가 다른 것 한 개를 골라보자
 print("you" "need" "python")
@@ -46,37 +50,49 @@ print("".join(["you", "need", "python"]))
 #출력하는 프로그램이다.
 
 f1 = open("test.txt", 'w')
+f3 = open("test1.txt", 'w')
 f1.write("Life is too short")
-#f1.close()
+f3.write("Life is too short")
+f1.close()
+f3.close()
 
 f2 = open("test.txt", 'r')
+f4 = open("test1.txt", 'r')
 print(f2.read())
+print(f4.read())
 #f2.close()
 
 #Q6 사용자의 입력을 파일(test.txt)에 저장하는 프로그램을 작성해 보자.(단 프로그램을 다시 실행하더라도
 # 기존에 작성한 내용을 유지하고 새로 입력한 내용을 추가해야 한다.)
 
 user_input = input("저장할 내용을 입력하세요:")
-f = open('test.txt', 'a') #내용을 추가하기 위해 'a'를 사용
+f = open('test1.txt', 'a') #내용을 추가하기 위해 'a'를 사용
+#f.write("\n")
 f.write(user_input)
-f.write("\n")     #"개행"
+#f.write("\n")     #"개행"
 f.close()   
+
 
 #Q7 다음과 같은 내용을 지닌 파일 test.txt가 있다. 이 파일의 내용 중 'java'라는 문자열을
 #'python'으로 바꾸어서 저장해 보자
 
-f = open('test.txt', 'r')
+f = open('test1.txt', 'r')
 body = f.read()   #test.txt 파일의 내용을 body 변수에 저장
 f.close()
 
-with open('test.txt', 'r') as f:
+with open('test1.txt', 'r') as f:
   body = f.read()
 
-body = body.replace('python', 'java')
+with open('test1.txt', 'r') as f:
+  body = f.read()
 
-f = open('test.txt', 'w')
+
+body = body.replace('climbing', 'Life')
+#id(body)
+
+f = open('test1.txt', 'w')
 f.write(body)
 f.close()
 
-with open('test.txt', 'w') as f:
+with open('test1.txt', 'w') as f:
   f.write(body)
